8fb3c0fc68675bcf7309697320dffeb39a6aa301
julia <juliamarietrudel06@gmail.com>
2024-06-05 16:11:19 -0400

    debug email sending

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -1,7 +1,7 @@
 class MainMailer < ApplicationMailer
   def participation_invite(player)
     @player = player
-    Rails.logger.info "Sending email to #{@player.email}"
+    Rails.logger.info "Trying to send email to #{@player.email}"
     mail(to: @player.email, subject: 'Upcoming Games Participation')
     Rails.logger.info "Email sent to #{@player.email}"
   end
