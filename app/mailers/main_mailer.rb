8550dabcba0814d637a1925c0773b9291700df53
julia <juliamarietrudel06@gmail.com>
2024-06-05 16:45:59 -0400

    Add logging for generated email URLs

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -2,6 +2,8 @@ class MainMailer < ApplicationMailer
   def participation_invite(player)
     @player = player
     Rails.logger.info "Trying to send email to #{@player.email}"
+    @url = url_for(controller: 'games', action: 'index', token: @player.token, only_path: false)
+    Rails.logger.info "Generated URL for player #{@player.email}: #{@url}"
     mail(to: @player.email, subject: 'Upcoming Games Participation')
     Rails.logger.info "Email sent to #{@player.email}"
   end
