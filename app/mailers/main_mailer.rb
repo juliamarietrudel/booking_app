0b654790d6be8b06483e9d014648b50f85a5b82c
julia <juliamarietrudel06@gmail.com>
2024-06-05 15:34:13 -0400

    Optimize Puma configuration and add logging for email delivery

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -1,6 +1,8 @@
 class MainMailer < ApplicationMailer
   def participation_invite(player)
     @player = player
+    Rails.logger.info "Sending email to #{@player.email}"
     mail(to: @player.email, subject: 'Upcoming Games Participation')
+    Rails.logger.info "Email sent to #{@player.email}"
   end
 end
