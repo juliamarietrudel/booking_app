6ef79c786278f255ca275c9093fdedd7440053da
julia <juliamarietrudel06@gmail.com>
2024-06-05 16:35:32 -0400

    Update SMTP settings with App Password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,10 +103,10 @@ Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
       user_name: ENV['GMAIL_USERNAME'],
       password: ENV['GMAIL_PASSWORD'],
       domain: 'golfbookingapp.heroku.com',
-      address: 'smtp.sendgrid.net',
       port: 587,
       authentication: :plain,
       enable_starttls_auto: true
