97ba6068117046803df4ee3d4ce83da2deae53f4
julia <juliamarietrudel06@gmail.com>
2024-06-06 12:29:19 -0400

    all routes now work & changed message

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,4 @@ player = Player.create(
 )
 players << player
 
-# player = Player.create(
-#   name: 'Charles Desjardins',
-#   email: 'desjardins.charles.a@gmail.com',
-#   token: SecureRandom.urlsafe_base64
-# )
-# players << player
-
 puts "#{players.count} players added"
