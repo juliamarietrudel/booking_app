1193780a8088e5a64e229b690d00518fdc25988b
julia <juliamarietrudel06@gmail.com>
2024-06-09 20:17:11 -0400

    updated seeds;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,5 @@
 require 'faker'
 
-# games = []
-
-# games << Game.create(date: Date.parse('10.06.2024'))
-# games << Game.create(date: Date.parse('11.06.2024'))
-# games << Game.create(date: Date.parse('12.06.2024'))
-# games << Game.create(date: Date.parse('13.06.2024'))
-
-puts 'four games were created'
-
 players = []
 
 player = Player.create(
@@ -20,12 +11,27 @@ player = Player.create(
 players << player
 
 player = Player.create(
-  first_name: 'Charles',
-  last_name: 'Desjardins',
-  email: 'desjardins.charles.a@gmail.com',
+  first_name: 'Vincent',
+  last_name: 'Trudel',
+  email: 'trudelvincent03@videotron.ca',
   token: SecureRandom.urlsafe_base64
 )
 players << player
 
+player = Player.create(
+  first_name: 'Nathalie',
+  last_name: 'Denault',
+  email: 'natdenault@videotron.ca',
+  token: SecureRandom.urlsafe_base64
+)
+players << player
+
+player = Player.create(
+  first_name: 'Aurelie',
+  last_name: 'Trudel',
+  email: 'aurelietrudel@gmail.com',
+  token: SecureRandom.urlsafe_base64
+)
+players << player
 
 puts "#{players.count} players added"
