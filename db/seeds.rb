4c45b67877afc629b33ef962dbd91a5e01860e0e
julia <juliamarietrudel06@gmail.com>
2024-06-09 20:48:25 -0400

    added vincent outlook email

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,37 +2,13 @@ require 'faker'
 
 players = []
 
-# player = Player.create(
-#   first_name: 'Julia',
-#   last_name: 'Trudel',
-#   email: 'juliamarietrudel96@gmail.com',
-#   token: SecureRandom.urlsafe_base64
-# )
-# players << player
-
-# player = Player.create(
-#   first_name: 'Vincent',
-#   last_name: 'Trudel',
-#   email: 'trudelvincent03@videotron.ca',
-#   token: SecureRandom.urlsafe_base64
-# )
-# players << player
-
-# player = Player.create(
-#   first_name: 'Nathalie',
-#   last_name: 'Denault',
-#   email: 'natdenault@videotron.ca',
-#   token: SecureRandom.urlsafe_base64
-# )
-# players << player
-
-# player = Player.create(
-#   first_name: 'Aurelie',
-#   last_name: 'Trudel',
-#   email: 'aurelietrudel@gmail.com',
-#   token: SecureRandom.urlsafe_base64
-# )
-# players << player
+player = Player.create(
+  first_name: 'Julia',
+  last_name: 'Trudel',
+  email: 'juliamarietrudel96@gmail.com',
+  token: SecureRandom.urlsafe_base64
+)
+players << player
 
 player = Player.create(
   first_name: 'Vincent',
@@ -40,7 +16,24 @@ player = Player.create(
   email: 'trudelvincent@outlook.com',
   token: SecureRandom.urlsafe_base64
 )
-
 players << player
+
+player = Player.create(
+  first_name: 'Nathalie',
+  last_name: 'Denault',
+  email: 'natdenault@videotron.ca',
+  token: SecureRandom.urlsafe_base64
+)
+players << player
+
+player = Player.create(
+  first_name: 'Aurelie',
+  last_name: 'Trudel',
+  email: 'aurelietrudel@gmail.com',
+  token: SecureRandom.urlsafe_base64
+)
+players << player
+
+
 
 puts "#{players.count} players added"
